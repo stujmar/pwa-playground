335fc4f619809fcab1dee46e02e7a8b12ba68adc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-29 18:11:05 -0600

    [sm] got bogged down playing with the color classes in materializeCSS

--- js/color-grid.js
+++ js/color-grid.js
@@ -1,6 +1,27 @@
 console.log(document)
 let colorGrid = document.getElementById('color-grid');
-myDiv = "<div>hello world</div>"
-colorGrid.setAttribute('style', 'display: grid; height: 10px; width: 10px; border: 2px solid red;');
-colorGrid.innerHTML = myDiv;
+let cheeseDiv = '<div id="cheese"></div>';
+colorGrid.setAttribute('style', 'display: flex; width: 100%; flex-wrap: wrap; border: 0px solid red;');
+colorGrid.innerHTML = cheeseDiv;
 console.log(colorGrid);
+
+let squareStyle = `style='display: grid; height: 20px; width: 20px;'`
+
+function addFive(divId, color) {
+  let darkDiv = (i) => `<div ${squareStyle} class="${color} darken-${i}"></div>`;
+  let myDiv = `<div ${squareStyle} class="${color}"></div>`;
+  let lightDiv = (i) => `<div ${squareStyle} class="${color} lighten-${i}"></div>`;
+  for (let i = -4; i < 6; i++) {
+    if (i <0) {
+      colorGrid.innerHTML += darkDiv(Math.abs(i));
+    } else if (i === 0) {
+      colorGrid.innerHTML += myDiv;
+    } else {
+      colorGrid.innerHTML += lightDiv(i);
+    }
+  }
+}
+
+let materializeColors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'yellow', 'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'];
+
+materializeColors.forEach(color => addFive(colorGrid, color));
