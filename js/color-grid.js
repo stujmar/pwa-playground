daa091933bab91fe6307ff83c2ada4f8eda4249d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-29 18:17:34 -0600

    [sm] have a loop with a delay and access to the interator within the delay

--- js/color-grid.js
+++ js/color-grid.js
@@ -25,3 +25,19 @@ function addFive(divId, color) {
 let materializeColors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'yellow', 'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'];
 
 materializeColors.forEach(color => addFive(colorGrid, color));
+
+let shrinkGrow = document.getElementById('shrink-grow');
+
+var i = 1;                  //  set your counter to 1
+
+function myLoop() {         //  create a loop function
+  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+    console.log('hello', i);   //  your code here
+    i++;                    //  increment the counter
+    if (i < 10) {           //  if the counter < 10, call the loop function
+      myLoop();             //  ..  again which will trigger another 
+    }                       //  ..  setTimeout()
+  }, 1000)
+}
+
+myLoop();                   //  start the loop
