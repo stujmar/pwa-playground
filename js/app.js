d01f1446035641d0caf2070be3a29df93ad5c757
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-07 22:11:46 -0600

    [sm] fixed typo from tutorial and now logging the promise from the service worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Check if service worker is supported
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    console.log('Service Worker Registered'), registration;
+    console.log('Service Worker Registered', registration);
   }).catch(function(err) {
     console.log('Service Worker Failed to Register', err);
   });
