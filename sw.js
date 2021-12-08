3af0b46d8283161974ca7feba3ed3b743cab791d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-07 22:42:47 -0600

    [sm] added event listener to service worker to log its installation ... successfully

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-console.log('Hello from the service worker.');
+// console.log('Hello from the service worker.');
+
+self.addEventListener('install', evt => {
+  console.log('The service worker is being installed.');
+})
