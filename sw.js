b556dbb53294317aeed873b3c2a73f93470c263b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-08 18:23:25 -0600

    [sm] added event listeners for activate event on service worker, successfully

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 // console.log('Hello from the service worker.');
 
 self.addEventListener('install', evt => {
-  console.log('The service worker is being installed.');
-})
+  console.log('The service worker is being installed.', evt);
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('The service worker is being activated.', evt);
+});
