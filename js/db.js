96ab4a54fc7ee704547314161d9239f1434de583
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-09 22:19:20 -0600

    [sm] added eventlistener for form submit and started building data object to save to db

---
+++ js/db.js
@@ -0,0 +1,23 @@
+
+
+
+// Adding new contact.
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  console.log('submit attempt');
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const phone = document.querySelector('#phone').value;
+    const email = document.querySelector('#email').value;
+    const address = document.querySelector('#address').value;
+    const newContact = {
+        name,
+        phone,
+        email,
+        address
+    };
+    db.collection('contacts').add(newContact);
+    console.log(newContact);
+    form.reset();
+});
