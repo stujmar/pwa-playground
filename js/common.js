8af2b2bdae2075164ffc13a8bc77014de95c111c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-10 21:16:00 -0600

    [sm] added an API folder and a products.json with some dummy data in it

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
+// import moment from 'moment';
+// var moment = require('moment'); // require
+// console.log("moment:", moment().format());
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
