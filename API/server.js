0d1acb4b32f6fd25f4ac2396c9d22926fc70c660
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-10 21:52:07 -0600

    [sm] got server running and logging which port it is using

--- API/server.js
+++ API/server.js
@@ -10,4 +10,5 @@ const server = http.createServer((req, res) => {})
 // If there is, it will use that port number.
 // If there isn't, it will use port 5000.
 const PORT = process.env.PORT || 5000;
-server.listen(PORT);
+// console.log(process.env);
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
