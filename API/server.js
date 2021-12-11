f1d5c4be8ebbf3c8b875a574da42f3ae39472cd1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-11 16:21:26 -0600

    [sm] added a else catch all to return a 404 on our vanilla js node api

--- API/server.js
+++ API/server.js
@@ -20,6 +20,9 @@ const server = http.createServer((req, res) => {
   if (req.url === '/api/products') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Page not found</h1>');
   }
 });
 
