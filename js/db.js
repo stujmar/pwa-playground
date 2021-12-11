229bae9b3ae9ed8c5122b418db2988d0b03ed672
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-10 20:18:46 -0600

    [sm] got firestore importing outside of module error but no double intializeApp var error, wip

--- js/db.js
+++ js/db.js
@@ -9,15 +9,9 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.querySelector('#title').value;
     const phone = document.querySelector('#numbers').value;
-    // const name = document.querySelector('#name').value;
-    // const phone = document.querySelector('#phone').value;
-    // const email = document.querySelector('#email').value;
-    // const address = document.querySelector('#address').value;
     const newContact = {
         name,
         phone,
-        // email,
-        // address
     };
     db.collection('contacts').add(newContact);
     console.log(newContact);
