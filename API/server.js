6251420005aee38157c88392b8850a7479de7657
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-11 16:48:43 -0600

    [sm] made it so the request method needs to be GET in order for the products info to be returned

--- API/server.js
+++ API/server.js
@@ -17,7 +17,7 @@ const oldServer = http.createServer((req, res) => {
 })
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/products') {
+  if (req.url === '/api/products' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(products));
   } else {
