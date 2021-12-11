8abcfbd1c4b4134dff387635b10ffb4eb35bdd1b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-10 19:51:39 -0600

    [sm] reworked for submit logic to only have the title and number fields like in the app

--- js/db.js
+++ js/db.js
@@ -7,15 +7,17 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
   console.log('submit attempt');
     e.preventDefault();
-    const name = document.querySelector('#name').value;
-    const phone = document.querySelector('#phone').value;
-    const email = document.querySelector('#email').value;
-    const address = document.querySelector('#address').value;
+    const name = document.querySelector('#title').value;
+    const phone = document.querySelector('#numbers').value;
+    // const name = document.querySelector('#name').value;
+    // const phone = document.querySelector('#phone').value;
+    // const email = document.querySelector('#email').value;
+    // const address = document.querySelector('#address').value;
     const newContact = {
         name,
         phone,
-        email,
-        address
+        // email,
+        // address
     };
     db.collection('contacts').add(newContact);
     console.log(newContact);
