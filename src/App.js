621235988e11c3b75924b72fc73e0b034537bee0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-14 21:58:05 -0600

    [sm] made a cool font from googles come in as a custom class, very cool

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <div className="w-full border-b-8 border-rose-400"></div>
         <div className="w-full border-b-8 border-stone-300"></div>
         <div className="w-full border-b-8 border-lime-300"></div>
-        <div className="bg-lime-100">Hello World</div>
+        <div className="bg-lime-100 josefin pt-1 text-lime-900">Hello World</div>
         <div className="w-full border-b-8 border-lime-300"></div>
         <div className="w-full border-b-8 border-stone-300"></div>
         <div className="w-full border-b-8 border-rose-400"></div>
