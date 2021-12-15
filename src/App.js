b5adee30c3b6d2bb11feb0a266ab4cf13e0c8aba
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-14 21:47:36 -0600

    [sm] got tailwind classes up and running

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="bg-gray-100 w-full h-screen pt-2">
+      <div className="text-center text-2xl font-bold">
+        Hello World
+      </div>
     </div>
   );
 }
