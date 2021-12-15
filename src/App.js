974be19c62886e2acd3bd5c866a6d0d0b6ce999a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-14 21:54:49 -0600

    [sm] did some cool custom colors without imports very nice tailwind 3

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-gray-100 w-full h-screen pt-2">
+    <div className="bg-gray-100 w-full h-screen pt-12">
       <div className="text-center text-2xl font-bold">
-        Hello World
+        <div className="w-full border-b-8 border-rose-400"></div>
+        <div className="w-full border-b-8 border-stone-300"></div>
+        <div className="w-full border-b-8 border-lime-300"></div>
+        <div className="bg-lime-100">Hello World</div>
+        <div className="w-full border-b-8 border-lime-300"></div>
+        <div className="w-full border-b-8 border-stone-300"></div>
+        <div className="w-full border-b-8 border-rose-400"></div>
       </div>
     </div>
   );
