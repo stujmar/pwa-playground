5f545c6557b513ca8d2137b3ff326bae73eef34a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-15 22:39:25 -0600

    [sm] brought HelloBar and NavBar into App.js and adjusted padding accordingly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
+import HelloBar from './components/HelloBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="bg-gray-100 w-full h-screen pt-12">
-      <div className="text-center text-2xl font-bold">
-        <div className="w-full border-b-8 border-rose-400"></div>
-        <div className="w-full border-b-8 border-stone-300"></div>
-        <div className="w-full border-b-8 border-lime-300"></div>
-        <div className="bg-lime-100 josefin pt-1 text-lime-900">Hello World</div>
-        <div className="w-full border-b-8 border-lime-300"></div>
-        <div className="w-full border-b-8 border-stone-300"></div>
-        <div className="w-full border-b-8 border-rose-400"></div>
-      </div>
+    <div className="pt-12">
+      <HelloBar />
+      <NavBar />
     </div>
   );
 }
