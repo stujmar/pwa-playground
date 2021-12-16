a2780a755551b0a23d78541f05e234a58805aaa6
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-15 22:54:15 -0600

    [sm] made a duplicate of hello bar and called it hello beam

---
+++ src/components/HelloBeam.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HelloBeam = () => {
+  return (
+    <div className="text-center text-2xl font-bold">
+      <div className="w-full border-b-8 border-rose-400"></div>
+      <div className="w-full border-b-8 border-stone-300"></div>
+      <div className="w-full border-b-8 border-lime-300"></div>
+      <div className="bg-lime-100 josefin pt-1 text-lime-900">Hello World</div>
+      <div className="w-full border-b-8 border-lime-300"></div>
+      <div className="w-full border-b-8 border-stone-300"></div>
+      <div className="w-full border-b-8 border-rose-400"></div>
+    </div>
+  )
+}
+
+export default HelloBeam;
