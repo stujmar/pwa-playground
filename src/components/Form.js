d4e4ae46aaf4f7111b61454352e0b4e146a5b858
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-15 23:03:58 -0600

    [sm] started to restyle form to match the colorful nature of the rest of the page

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = () => {
   return (
   <div class="w-full max-w-xs mt-12">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form class="bg-white shadow-md shadow-sky-400 rounded-lg px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
           Username
@@ -18,10 +18,10 @@ const Form = () => {
         <p class="text-red-500 text-xs italic">Please choose a password.</p>
       </div>
       <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
           Sign In
         </button>
-        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/">
+        <a class="inline-block align-baseline font-bold text-sm text-sky-500 hover:text-sky-800" href="/">
           Forgot Password?
         </a>
       </div>
