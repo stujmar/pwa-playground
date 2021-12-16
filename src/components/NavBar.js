51b2c8527fe46aade852a68cf0c7894ec3f7f7a1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-15 22:36:23 -0600

    [sm] roughed out a NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <a href="!#">ONE</a>
+      <a href="!#">TWO</a>
+      <a href="!#">THREE</a>
+    </div>
+  )
+}
+
+export default NavBar;
