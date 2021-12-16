8d26578fd04c72ba6f07bd483a9db174f499d6dd
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-15 23:00:27 -0600

    [sm] build some wrapping paper breams to hold up the hellobar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import HelloBar from './components/HelloBar';
+import HelloBeam from './components/HelloBeam';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="pt-12">
+    <div className="pt-12 bg-sky-50">
       <HelloBar />
-      <NavBar />
+      <div className="flex justify-around items-start">
+        <HelloBeam />
+        <NavBar />
+        <HelloBeam />
+      </div>
     </div>
   );
 }
--- src/components/HelloBeam.js
+++ src/components/HelloBeam.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const HelloBeam = () => {
   return (
-    <div className="text-center text-2xl font-bold">
-      <div className="w-full border-b-8 border-rose-400"></div>
-      <div className="w-full border-b-8 border-stone-300"></div>
-      <div className="w-full border-b-8 border-lime-300"></div>
-      <div className="bg-lime-100 josefin pt-1 text-lime-900">Hello World</div>
-      <div className="w-full border-b-8 border-lime-300"></div>
-      <div className="w-full border-b-8 border-stone-300"></div>
-      <div className="w-full border-b-8 border-rose-400"></div>
+    <div className="text-center text-2xl font-bold flex">
+      <div className="h-screen border-l-8 border-rose-400"></div>
+      <div className="h-screen border-l-8 border-stone-300"></div>
+      <div className="h-screen border-l-8 border-lime-300"></div>
+      <div className="bg-lime-100 w-10 pt-1 text-lime-900"></div>
+      <div className="h-screen border-l-8 border-lime-300"></div>
+      <div className="h-screen border-l-8 border-stone-300"></div>
+      <div className="h-screen border-l-8 border-rose-400"></div>
     </div>
   )
 }
