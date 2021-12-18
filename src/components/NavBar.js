d14ed80b102cf355392782af6cd00afcd45ad372
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-18 16:49:51 -0600

    [sm] reworked the dropshadows and bevels on the buttons via tailwind3 very cool

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,26 +5,33 @@ const NavBar = () => {
     <div className="flex justify-center gap-x-12 mt-12">
       <a 
         href="!#"
-        className="bg-rose-500 shadow-lg shadow-rose-500 josefin px-4 py-2 rounded-full
-        underline underline-offset-2 decoration-2 decoration-rose-100
-        border-t-2 border-t-rose-300     
-        border-b-4 border-b-rose-600 
+        className="bg-rose-500 shadow-md shadow-rose-500/50 josefin px-4 pt-0.5     
+        border-t-2 border-t-rose-300/60   
+        border-b-2 border-b-rose-600/40
         text-rose-50 font-bold hover:bg-rose-600"
       >ROSE</a>
       <a 
         href="!#"
-        className="bg-sky-500 shadow-lg shadow-sky-500 josefin px-4 py-2 rounded-full
-        underline underline-offset-2 decoration-2 decoration-sky-100
-        border-t-2 border-t-sky-300     
-        border-b-4 border-b-sky-600 
+        className="bg-sky-500 shadow-md shadow-sky-500/50 josefin px-4 pt-0.5
+    
+        border-t-2 border-t-sky-400
+        border-b-2 border-b-sky-600
         text-sky-50 font-bold hover:bg-sky-600"
-      >SKY</a>
+      >CLICK ME</a>
       <a 
         href="!#"
-        className="bg-lime-500 shadow-lg shadow-lime-500 josefin px-4 py-2 rounded-full
-        underline underline-offset-2 decoration-2 decoration-lime-100
-        border-t-2 border-t-lime-300     
-        border-b-4 border-b-lime-600 
+        className="bg-sky-500 shadow-md shadow-sky-500/50 josefin px-4 pt-0.5
+        
+        border-t-2 border-t-sky-400/60
+        border-b-2 border-b-sky-600/40 
+        text-sky-50 font-bold hover:bg-sky-600"
+      >CLICK ME</a>
+      <a 
+        href="!#"
+        className="bg-lime-500 shadow-lg shadow-lime-500/50 josefin px-4 pt-0.5 
+        
+        border-t-2 border-t-lime-300/60     
+        border-b-2 border-b-lime-600/40
         text-lime-50 font-bold hover:bg-lime-600"
       >LIME</a>
     </div>
