4e84d5c18480fbbd0462536ed317381d7cbb49e3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-19 21:51:02 -0600

    [sm] added the Outlet element in the Home component for child routes to be rendered

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelloBar from '../HelloBar';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="pt-12">
       <HelloBar message="Welcome Home" />
+      <Outlet />
     </div>
   )
 }
