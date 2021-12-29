e5281cb08f6d4209e30474638361c22258b45b0d
Stuart Marsh <stujmar@gmail.com>
2021-12-29 16:59:40 -0500

    [sm] used useParams so the singles have access to the current team ids

--- src/components/teams/Team.js
+++ src/components/teams/Team.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Team = ({data}) => {
+
+  let params = useParams();
+
   return (
     <div className="m-2 border rounded-sm">
       this is a single team/invoice
-      Item: {data.name}</div>
+      Item: {params.id}</div>
   )
 }
 
