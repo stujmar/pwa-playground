4b8e080452dbd88229c12c0e46d701782686d84c
Stuart Marsh <stujmar@gmail.com>
2021-12-30 17:11:38 -0500

    [sm] made a crude form to create new items

--- src/components/teams/NewItemForm.js
+++ src/components/teams/NewItemForm.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const NewItemForm = () => {
   return (
-    <div>New Item Form</div>
+    <div className="mx-12">New Item Form
+      <form className="flex flex-col gap-2 item-start">
+        <div>id: 4</div>
+        <input className="border w-max pl-2" type="text" placeholder="name" />
+        <input className="border w-max pl-2" type="text" placeholder="description" />
+        <input className="border w-max pl-2" type="number" placeholder="price" />
+      </form>
+    </div>
   )
 }
 
