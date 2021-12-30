ea8589a3e19c33ec59b7c59b999220c6f8c9a2fe
Stuart Marsh <stujmar@gmail.com>
2021-12-29 19:42:36 -0500

    [sm] finally got the slug adn the single to match and display content from the filtered data

--- src/components/teams/Team.js
+++ src/components/teams/Team.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Team = ({data}) => {
 
+  const [invoice, setInvoice] = useState({});
   let params = useParams();
+  let test = data.filter((item) => {
+    console.log(parseInt(item.id) === parseInt(params.id));
+   return parseInt(item.id) === parseInt(params.id);
+  })[0];
+
+  console.log("test", test);
+  useEffect(() => {
+    console.log(data, params.id);
+    
+    setInvoice(data.filter((item) => {
+      console.log(parseInt(item.id) === parseInt(params.id));
+     return parseInt(item.id) === parseInt(params.id);
+    })[0]);
+  },[])
+
 
   return (
-    <div className="m-2 border rounded-sm">
-      this is a single team/invoice
-      Item: {params.id}</div>
+    <div className="m-2 border rounded-sm flex flex-col">
+      <div>Item: {params.id}</div>
+      <div>{invoice.name}</div>
+      <div>{invoice.description}</div>
+      
+      </div>
   )
 }
 
--- src/components/teams/TeamCard.js
+++ src/components/teams/TeamCard.js
@@ -3,7 +3,6 @@ import React from 'react';
 const TeamCard = ({data, onClick}) => {
 
   const handleClick = () => {
-    console.log(data.id);
     onClick(data.id);
   }
   return (
